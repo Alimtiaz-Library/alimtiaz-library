6fb31783df20e3e0378de84df9e487b49b9234f4
Saadawii <msa3dawii@gmail.com>
2026-04-12 23:09:51 +0200

    إضافة مواد الفرقة الرابعة شريعة بالكامل وتنشيط المذهب المالكي

--- data/sharia/year4.js
+++ data/sharia/year4.js
@@ -1,12 +1,60 @@
 window.AlimtiazData = window.AlimtiazData || {};
 window.AlimtiazData['sharia_4_2'] = {
     // الفرقة الرابعة - الفصل الدراسي الثاني
-    "حديث الأحكام":          { summary: "", essay: "", tf: "", full: "", book: "" },
-    "أصول الفقه الحنفي":     { summary: "", essay: "", tf: "", full: "", book: "" },
-    "أصول الفقه الغير حنفي": { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مقارن":             { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي حنفي":        { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي شافعي":       { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي مالكي":       { summary: "", essay: "", tf: "", full: "", book: "" },
-    "قضايا فقهية معاصرة":    { summary: "", essay: "", tf: "", full: "", book: "" }
+    "حديث الأحكام": {
+        summary: "sh4_hadahk_sum",
+        essay: "sh4_hadahk_ess",
+        tf: "sh4_hadahk_tf",
+        full: "sh4_hadahk_full",
+        book: "sh4_hadahk_book"
+    },
+    "أصول الفقه الحنفي": {
+        summary: "يتوفر قريباً",
+        essay: "يتوفر قريباً",
+        tf: "يتوفر قريباً",
+        full: "يتوفر قريباً",
+        book: "يتوفر قريباً"
+    },
+    "أصول الفقه الغير حنفي": {
+        summary: "يتوفر قريباً",
+        essay: "يتوفر قريباً",
+        tf: "يتوفر قريباً",
+        full: "يتوفر قريباً",
+        book: "يتوفر قريباً"
+    },
+    "فقه مقارن": {
+        summary: "sh4_fqhmuq_sum",
+        essay: "sh4_fqhmuq_ess",
+        tf: "sh4_fqhmuq_tf",
+        full: "sh4_fqhmuq_full",
+        book: "sh4_fqhmuq_book"
+    },
+    "فقه مذهبي حنفي": {
+        summary: "sh4_fqhhan_sum",
+        essay: "sh4_fqhhan_ess",
+        tf: "sh4_fqhhan_tf",
+        full: "sh4_fqhhan_full",
+        book: "sh4_fqhhan_book"
+    },
+    "فقه مذهبي شافعي": {
+        summary: "sh4_fqhsha_sum",
+        essay: "sh4_fqhsha_ess",
+        tf: "sh4_fqhsha_tf",
+        full: "sh4_fqhsha_full",
+        book: "sh4_fqhsha_book"
+    },
+    "فقه مذهبي مالكي": {
+        summary: "sh4_fqhmal_sum",
+        essay: "sh4_fqhmal_ess",
+        tf: "sh4_fqhmal_tf",
+        full: "sh4_fqhmal_full",
+        book: "sh4_fqhmal_book"
+    },
+    "قضايا فقهية معاصرة": {
+        summary: "sh4_qad_sum",
+        essay: "sh4_qad_ess",
+        tf: "sh4_qad_tf",
+        full: "sh4_qad_full",
+        book: "sh4_qad_book"
+    }
 };
