9252afecc498f2d85e934edf76b2833b2dd081c1
Saadawii <msa3dawii@gmail.com>
2026-04-19 14:04:39 +0200

    إضافة مواد وتفعيل الفرقة الثانية شريعة

--- data/sharia/year2.js
+++ data/sharia/year2.js
@@ -1,12 +1,60 @@
 window.AlimtiazData = window.AlimtiazData || {};
 window.AlimtiazData['sharia_2_2'] = {
     // الفرقة الثانية - الفصل الدراسي الثاني
-    "حديث الأحكام":              { summary: "", essay: "", tf: "", full: "", book: "" },
-    "أصول الفقه الحنفي":         { summary: "", essay: "", tf: "", full: "", book: "" },
-    "أصول الفقه الغير حنفي":     { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مقارن":                 { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي حنفي":            { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي شافعي":           { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي مالكي":           { summary: "", essay: "", tf: "", full: "", book: "" },
-    "أحوال الشخصية للمسلمين":    { summary: "", essay: "", tf: "", full: "", book: "" }
+    "حديث الأحكام": {
+        summary: "sh2_hadahk_sum",
+        essay: "sh2_hadahk_ess",
+        tf: "sh2_hadahk_tf",
+        full: "sh2_hadahk_full",
+        book: "sh2_hadahk_book"
+    },
+    "أصول الفقه الحنفي": {
+        summary: "يتوفر قريباً",
+        essay: "يتوفر قريباً",
+        tf: "يتوفر قريباً",
+        full: "يتوفر قريباً",
+        book: "يتوفر قريباً"
+    },
+    "أصول الفقه الغير حنفي": {
+        summary: "sh2_usloth_sum",
+        essay: "sh2_usloth_ess",
+        tf: "sh2_usloth_tf",
+        full: "sh2_usloth_full",
+        book: "sh2_usloth_book"
+    },
+    "فقه مقارن": {
+        summary: "sh2_fqhmuq_sum",
+        essay: "sh2_fqhmuq_ess",
+        tf: "sh2_fqhmuq_tf",
+        full: "sh2_fqhmuq_full",
+        book: "sh2_fqhmuq_book"
+    },
+    "فقه مذهبي حنفي": {
+        summary: "يتوفر قريباً",
+        essay: "يتوفر قريباً",
+        tf: "يتوفر قريباً",
+        full: "يتوفر قريباً",
+        book: "يتوفر قريباً"
+    },
+    "فقه مذهبي شافعي": {
+        summary: "يتوفر قريباً",
+        essay: "يتوفر قريباً",
+        tf: "يتوفر قريباً",
+        full: "يتوفر قريباً",
+        book: "يتوفر قريباً"
+    },
+    "فقه مذهبي مالكي": {
+        summary: "يتوفر قريباً",
+        essay: "يتوفر قريباً",
+        tf: "يتوفر قريباً",
+        full: "يتوفر قريباً",
+        book: "يتوفر قريباً"
+    },
+    "أحوال الشخصية للمسلمين": {
+        summary: "sh2_ahwper_sum",
+        essay: "sh2_ahwper_ess",
+        tf: "sh2_ahwper_tf",
+        full: "sh2_ahwper_full",
+        book: "sh2_ahwper_book"
+    }
 };
