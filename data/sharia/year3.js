b2a2278131ff9e5ce8e3cce04a626d434d886cd5
Saadawii <msa3dawii@gmail.com>
2026-05-02 12:55:07 +0300

    إضافة مواد وتفعيل الفرقة الثالثة شريعة

--- data/sharia/year3.js
+++ data/sharia/year3.js
@@ -1,12 +1,60 @@
 window.AlimtiazData = window.AlimtiazData || {};
 window.AlimtiazData['sharia_3_2'] = {
     // الفرقة الثالثة - الفصل الدراسي الثاني
-    "حديث الأحكام":          { summary: "", essay: "", tf: "", full: "", book: "" },
-    "أصول الفقه الحنفي":     { summary: "", essay: "", tf: "", full: "", book: "" },
-    "أصول الفقه الغير حنفي": { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مقارن":             { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي حنفي":        { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي شافعي":       { summary: "", essay: "", tf: "", full: "", book: "" },
-    "فقه مذهبي مالكي":       { summary: "", essay: "", tf: "", full: "", book: "" },
-    "قضايا فقهية معاصرة":    { summary: "", essay: "", tf: "", full: "", book: "" }
+    "حديث الأحكام": {
+        summary: "sh3_hadahk_sum",
+        essay: "sh3_hadahk_ess",
+        tf: "sh3_hadahk_tf",
+        full: "يتوفر قريباً",
+        book: "sh3_hadahk_book"
+    },
+    "أصول الفقه الحنفي": {
+        summary: "sh3_uslhan_sum",
+        essay: "sh3_uslhan_ess",
+        tf: "sh3_uslhan_tf",
+        full: "يتوفر قريباً",
+        book: "sh3_uslhan_book"
+    },
+    "أصول الفقه الغير حنفي": {
+        summary: "sh3_usloth_sum",
+        essay: "sh3_usloth_ess",
+        tf: "sh3_usloth_tf",
+        full: "يتوفر قريباً",
+        book: "sh3_usloth_book"
+    },
+    "فقه مقارن": {
+        summary: "sh3_fqhmuq_sum",
+        essay: "sh3_fqhmuq_ess",
+        tf: "sh3_fqhmuq_tf",
+        full: "يتوفر قريباً",
+        book: "sh3_fqhmuq_book"
+    },
+    "فقه مذهبي حنفي": {
+        summary: "sh3_fqhhan_sum",
+        essay: "sh3_fqhhan_ess",
+        tf: "sh3_fqhhan_tf",
+        full: "يتوفر قريباً",
+        book: "sh3_fqhhan_book"
+    },
+    "فقه مذهبي شافعي": {
+        summary: "sh3_fqhsha_sum",
+        essay: "sh3_fqhsha_ess",
+        tf: "sh3_fqhsha_tf",
+        full: "sh3_fqhsha_full",
+        book: "sh3_fqhsha_book"
+    },
+    "فقه مذهبي مالكي": {
+        summary: "sh3_fqhmal_sum",
+        essay: "sh3_fqhmal_ess",
+        tf: "sh3_fqhmal_tf",
+        full: "يتوفر قريباً",
+        book: "sh3_fqhmal_book"
+    },
+    "قضايا فقهية معاصرة": {
+        summary: "sh3_qad_sum",
+        essay: "sh3_qad_ess",
+        tf: "sh3_qad_tf",
+        full: "sh3_qad_full",
+        book: "sh3_qad_book"
+    }
 };
